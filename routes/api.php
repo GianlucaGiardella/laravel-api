2a95339f4f61c7cf4b618981f86e9680655f3a69
GianluGiarde <gianlugiarde@gmail.com>
2023-07-18 17:27:44 +0200

    add type controller

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\TypeController;
 use App\Http\Controllers\Api\ProjectController;
 
 /*
@@ -20,4 +21,7 @@ use App\Http\Controllers\Api\ProjectController;
 // });
 
 Route::get('projects', [ProjectController::class, 'index'])->name('api.projects.index');
+Route::get('projects/random', [ProjectController::class, 'random'])->name('api.projects.random');
 Route::get('projects/{project}', [ProjectController::class, 'show'])->name('api.projects.show');
+
+Route::get('types', [TypeController::class, 'index'])->name('api.types.index');
